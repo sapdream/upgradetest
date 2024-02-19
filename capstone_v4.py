2c77ccb6946ae9d6735e9e6bc3d9125078c32ac5
sapdream <156305094+sapdream@users.noreply.github.com>
2024-02-19 16:24:11 +0800

    Update capstone_v4.py

--- capstone_v4.py
+++ capstone_v4.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1GMBXFYKjHX039JjxVRZmZudAgpiHqwVb
 """
 
-!/bin/bash -m pip install --upgrade pip
-
 import streamlit as st
 import pickle
 import datetime

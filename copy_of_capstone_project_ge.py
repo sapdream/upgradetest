ba65527914cef8ef13fba9ed7580f5b5672c7e7c
sapdream <156305094+sapdream@users.noreply.github.com>
2024-02-19 14:53:12 +0800

    Update copy_of_capstone_project_ge.py

--- copy_of_capstone_project_ge.py
+++ copy_of_capstone_project_ge.py
@@ -20,11 +20,12 @@ def load_data():
 
 def getprediction(input_date):
 
-  
+  start_date = datetime.datetime(2024,2,19)
+  end_date = datetime.datetime(2025,12,12)
 
   #y_pred = model.get_forecast(len(test.index))
   #y_pred_df = y_pred.conf_int(alpha = 0.05)
-  y_pred_df["Predictions"] = model.predict(start = '2024-02-19', end = '2025-12-12')
+  y_pred_df["Predictions"] = model.predict(start = start_date, end = end_date)
   y_pred_df.index = input_date
   y_pred_out = y_pred_df["Predictions"]
   prediction = y_pred_out[input_date]

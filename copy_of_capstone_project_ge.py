582031d6d40db2df11b50dacb57ecbb3254f2bf3
sapdream <156305094+sapdream@users.noreply.github.com>
2024-02-19 14:16:22 +0800

    Update copy_of_capstone_project_ge.py

--- copy_of_capstone_project_ge.py
+++ copy_of_capstone_project_ge.py
@@ -105,7 +105,9 @@ def main():
   st.title('Clinic Peak Hour Prediction System')
   input_text = st.date_input("Enter Date to find busyness")
 
-  results = getprediction(input_text)
+  
+  if st.button('Predict'):
+      results = getprediction(input_text)
   st.markdown(results)
 
 if __name__ == "__main__":
